32f060a843d3e30de6d7e6bdfa3e5e36d1769090
syed-ahammad <Syed_Ahammad@outlook.com>
2022-09-29 23:52:57 +0600

    quatino added

---
+++ src/component/quations/Quation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Quation = () => {
+    return (
+        <div className='quation-container'>
+            <div className='quation'>
+                <h3>How does react work?</h3>
+                <p></p>
+            </div>
+        </div>
+    );
+};
+
+export default Quation;
