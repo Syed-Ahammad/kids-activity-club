19f109517d1f6074d52e22e48cc29a86f51cf66e
syed-ahammad <Syed_Ahammad@outlook.com>
2022-09-29 23:56:21 +0600

    activity details added

--- src/component/quations/Quation.js
+++ src/component/quations/Quation.js
@@ -5,8 +5,17 @@ const Quation = () => {
         <div className='quation-container'>
             <div className='quation'>
                 <h3>How does react work?</h3>
-                <p></p>
+                <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
             </div>
+            <div className='quation'>
+                <h3>How does react work?</h3>
+                <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
+            </div>
+            <div className='quation'>
+                <h3>What does different betWeen Props and state?</h3>
+                <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
+            </div>
+            
         </div>
     );
 };
